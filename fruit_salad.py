cff9bb83f3a07a4ca64cc6e59f8241f382892973
Alex Rocha <9896751+alexcrocha@users.noreply.github.com>
2022-12-03 14:03:52 -0800

    Update fruit_salad.py

--- fruit_salad.py
+++ fruit_salad.py
@@ -5,4 +5,3 @@ from rotten_fruit import RottenFruit
 class FruitSalad(TastyFruit, RottenFruit):
     def __init__(self, name, points, fall_speed, bonus, mood, illness):
         super(name, points, fall_speed, bonus, mood, illness)
-    pass
